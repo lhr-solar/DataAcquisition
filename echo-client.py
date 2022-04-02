4dac1433204539438c2ea7ac41128a90ac565357
talatcanoguz <talatcanoguz0@gmail.com>
2022-04-02 16:09:39 -0500

    made changes

--- echo-client.py
+++ echo-client.py
@@ -1,34 +1,20 @@
-#!/usr/bin/env python3
-#works with echo-server4
 import socket
 
 #HOST = socket.gethostbyname(socket.gethostname())
-HOST = "169.254.147.123"
+HOST = "169.254.48.219"
 PORT = 65432
 LENGTH = 64
 FORMAT = 'utf-8'
 DISCONNECT_MESSAGE = "DISCONNECT"
+BYTE = 1
 
 def send(msg):
 	with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
 		s.connect((HOST, PORT))
-		message = msg.encode(FORMAT)
-		msg_length = len(message)
-		send_length = str(msg_length).encode(FORMAT)
-		send_length += b' ' * (LENGTH - len(send_length))
-		s.send(send_length)
-		s.send(message)
-		print(s.recv(1024).decode(FORMAT))  
-		#encoding in a way that can be transmitted over ethernet
+		print("Hey")
+		s.send(msg)
+		print(s.recv(1024).decode(FORMAT))
 
-binary = input('enter a number: ')
-#for digit in binary:
-		#decimal = decimal*2 + int(digit)
-#number = int(binary, 2)
-send(binary)
-input()
-#send("DISCONNECT")
+send(bytes([0x1, 0x2, 0x3, 0x4]))		# b'\0x1\0x2'
+#send(DISCONNECT_MESSAGE)
 #print(f"Received {data!r}")
-#111100000001000000100000001000000001
-#=64441418241 is test number to send for bps trip
-#put 4 ones in front just to avoid the leading zeros issue
--- echo-server.py
+++ echo-server.py
@@ -1,61 +1,64 @@
 #!/usr/bin/env python3
-#run in putty
 import socket
-
 #HOST = socket.gethostbyname(socket.gethostname())
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 s.connect(("8.8.8.8", 80))
 print(s.getsockname()[0])
 HOST = s.getsockname()[0]
 PORT = 65432  # Port to listen on (non-privileged ports are > 1023)
-LENGTH = 64
+BYTE = 1
 FORMAT = 'utf-8'
 DISCONNECT_MESSAGE = "DISCONNECT"
-#multiplies to isolate byte identities
-xidentifier = str(111111111111000000000000000000000000) # item to mask for identifier, needs to be string for decima>
-xdatalength = str(111100000000111111110000000000000000) # data length multiplier to signify data to read
-xfirstdatabyte = str(111100000000000000001111111100000000)
-xseconddatabyte = str(111100000000000000000000000011111111)
-#data lengths
-datalengthis2 = str(111100000000000000100000000000000000)
-#all the CAN masks
-CANidentity = str(111100000001000000000000000000000000) #result if identity is CAN
-BPStripidentity = str(111100000000000000000000001000000000)
-BPStrip = str(111100000000000000000000000000000001)
+f = open("message.txt", "w")
+def IMUparse(data):
+    pass
+def GPSparse(data):
+    pass
+def RTCparse(data):
+    pass
+def CANparse(data):
+    pass
 def handle_client(conn, addr):
     print(f"Connected by {addr}")
-    connected = True
-    while connected:
-        msg_length = conn.recv(LENGTH).decode(FORMAT)
-        if msg_length:
-            msg_length = int(msg_length)
-            msg = conn.recv(msg_length).decode(FORMAT)
-            ryan = int(msg)
-            ryan1 = ryan & int(xidentifier, 2) #this is where mask occurs, need to convert binary to decimal for com>
-            if ryan1 ==  int(CANidentity, 2):
-                print(f"{addr} CAN.")
-                ryan2 = ryan & int(xdatalength, 2)
-                if ryan2 == int(datalengthis2, 2):
-                    print(f"{addr} Reading 2 bytes of data...")
-                    ryan3 = ryan & int(xfirstdatabyte, 2)
-                    if ryan3 == int(BPStripidentity,2):
-                        print(f"{addr} BPS trip")
-                        ryan4 = ryan & int(xseconddatabyte)
-                        if ryan4 == int(BPStrip, 2):
-                            print(f"{addr} BPS has tripped!")
-            if msg == DISCONNECT_MESSAGE:
-                print(f"{addr} disconnected.")
-                connected = False
-
-            print(f"{addr}: {ryan1}") #just to check what the outcome is
-            conn.send("Msg received". encode(FORMAT))
-
+    for i in range(0, 5):
+        f.write(str(conn.recv(BYTE)))
+    f.close()
+        #if id is None:
+        #    continue
+'''
+length = conn.recv(BYTE)
+length = int.from_bytes(length, "big")
+id = int.from_bytes(id, "big")
+data = conn.recv(length)
+if id == 1:
+    print(f"ID: IMU")
+    IMUparse(data)
+elif id == 2:
+    print(f"ID: GPS")
+    GPSparse(data)
+elif id == 3:
+    print(f"ID: RTC")
+    RTCparse(data)
+elif id == 4:
+    print(f"ID: CAN")
+    CANparse(data)
+else:
+    continue
+print(f"Length: {length}")
+print(f"Data: {data}")
+f.write("ID: " + str(id))
+f.write("\n")
+f.write("Length: " + str(length))
+f.write("\n")
+f.write("Data: " + str(data))
+'''
 def start():
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.bind((HOST, PORT))
         s.listen()
         print(f"Server listening on {HOST}")
         conn, addr = s.accept()
-
+        with conn:
+            handle_client(conn, addr)
 print("Server starting...")
 start()
