2471ae57afd72cfe9a6922e5839fbf99edd072c9
Andrew Nguyen <andrewvnguyen@utexas.edu>
2022-08-27 17:37:07 -0500

    telnet connection between docker containers, bad protocol

--- sim/src/main.py
+++ sim/src/main.py
@@ -1,24 +1,19 @@
 import socket
+import logging
 
-def server():
-    TCP_IP = 'localhost'
-    TCP_PORT = 65432
-    BUFFER_SIZE = 10  # Normally 1024, but we want fast response
+HOST = '10.5.0.5'
+PORT = 65432
 
+def sender():
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.bind((TCP_IP, TCP_PORT))
-    s.listen(1)
-
-    conn, addr = s.accept()
-    print('Connection address:', addr)
-    while 1:
-        data = conn.recv(BUFFER_SIZE)
-        if not data: break
-        print ("received data:", data)
-        conn.send(data)  # echo
+    s.connect((HOST, PORT))
+    logging.debug("Client starting...")
+    s.setblocking(True)
     
-    conn.close()
-
+    while True:
+        data = "test"
+        s.sendall(data.encode())
+        logging.debug("Data sent.")
         
 if __name__ == "__main__":
-    server()
+    sender()
