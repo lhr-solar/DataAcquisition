a3a9fb5c22f2f9aecd2972013a8733bea6e159d2
Andrew Nguyen <andrewvnguyen@utexas.edu>
2022-10-08 13:34:03 -0500

    imu parse fix + better CAN data

--- app/imu.py
+++ app/imu.py
@@ -8,7 +8,7 @@ def IMUparse(input):
     data = ["Accelerometer", "Magnetometer", "Gyroscope"]
     data_field = ['x', 'y', 'z']
     for i in range(0,9):
-        logging.debug(str(data[math.floor(i/3)]) + ' ' + str(data_field[i%3]) + ": " + str(int.from_bytes(input[(i*2):((i*2)+2)], "little", signed="True")))
+        logging.debug(data[math.floor(i/3)] + ' ' + data_field[i%3] + ": " + str(int.from_bytes(input[(i*2):((i*2)+2)], "little", signed = "True")))
     logging.debug("\n")
 
-    return [Point(data[math.floor(i/3)]).field(data_field[i%3], int.from_bytes(input[(i*2):((i*2)+2)], "little")) for i in range(0,9)]
+    return [Point(data[math.floor(i/3)]).field(data_field[i%3], int.from_bytes(input[(i*2):((i*2)+2)], "little", signed = "True")) for i in range(0,9)]
