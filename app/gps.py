50a0abca7fe79618601bec3986345462e58edf6c
Andrew Nguyen <andrewvnguyen@utexas.edu>
2022-11-06 13:48:18 -0600

    fixed gps parsing

--- app/gps.py
+++ app/gps.py
@@ -9,8 +9,8 @@ def GPSparse(data):
     #lat and lon is recognized by influxdb in order to show gui with map
     #converting GPS input to decimal degrees is required
     gps = { 'Status':       lambda i: i[9],
-            'lat':          lambda i: (float(i[10:14]) + float(i[15:19])/600000) * (-1 if i[30] == 'W' else 1), 
-            'lon':          lambda i: (float(i[20:25]) + float(i[26:30])/600000) * (-1 if i[19] == 'S' else 1),
+            'lat':          lambda i: (float(i[10:12]) + float(i[12:19])/60) * (-1 if i[19] == 'S' else 1), 
+            'lon':          lambda i: (float(i[20:23]) + float(i[23:30])/60) * (-1 if i[30] == 'W' else 1),
             'MPH':          lambda i: float(i[31:38]) * 1.15078
     }
     for key in gps:
