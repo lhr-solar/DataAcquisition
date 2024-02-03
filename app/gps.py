fa2bebc327a092f05d010adf5754c69a8b2072fa
Yejean Kim <kimyejean02@gmail.com>
2024-02-03 15:05:51 -0600

    integration testing

--- app/gps.py
+++ app/gps.py
@@ -3,17 +3,6 @@ import logging
 
 
 def GPSparse(data):
-    data = ['0' if i == 0 else chr(i) for i in data]
+    #TODO: Parse NMEA Sentence
     logging.debug(data)
-
-    #lat and lon is recognized by influxdb in order to show gui with map
-    #converting GPS input to decimal degrees is required
-    gps = { 'Status':       lambda i: i[9],
-            'lat':          lambda i: (float(i[10:12]) + float(i[12:18])/60) * (-1 if i[18] == 'S' else 1), 
-            'lon':          lambda i: (float(i[19:22]) + float(i[22:28])/60) * (-1 if i[28] == 'W' else 1),
-            'MPH':          lambda i: float(i[30:34]) * 1.15078
-    }
-    for key in gps:
-        logging.debug(str(key) + ": " + str(gps[key]("".join(data))))
     logging.debug("\n")
-    return [Point("GPS").field(key, gps[key](''.join(data))) for key in gps]
